6a794ad946c5047749755252c4e924142f37ecea
Pietro <67281154+pdemicheli@users.noreply.github.com>
2024-08-19 23:50:32 +0200

    Clean up deployment scripts

--- scripts/scratch-deployment/upgrade-native.js
+++ scripts/scratch-deployment/upgrade-native.js
@@ -1,6 +1,6 @@
 const { ethers, upgrades } = require("hardhat");
 
-const tokenAddress = "0xbd4c9fC10c099DfCeB17f866aeA1e9015C0a87F0";
+const tokenAddress = "";
 
 async function main() {
   const Token = await ethers.getContractFactory("SpaceNative");
